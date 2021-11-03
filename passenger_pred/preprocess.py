3d4874f81f6569ad193e7e2c48ee1ab1f39f1c7d
Lars Kouwenhoven <31851200+larskouwenhoven@users.noreply.github.com>
2021-11-03 14:57:36 -0400

    Add function add_time_features

--- passenger_pred/preprocess.py
+++ passenger_pred/preprocess.py
@@ -19,3 +19,18 @@ def remove_duplicate_stops(df):
     df = df.drop_duplicates(subset=['uid', 'next_stop_id'], keep='last', ignore_index=True)
     df = df.reset_index(drop=True)
     return df
+
+def add_time_features(df, timestamp_str="timestamp"):
+    """
+    Given a pd.DataFrame with a timestamp column, return a pd.DataFrame like df, but with
+    additional columns "hour", "day", "dow" (day of week)
+    
+    pd.DataFrame df
+    String timestamp_str: name of timestamp column in df
+    returns df
+    """
+    df['timestamp_dt'] = pd.to_datetime(df.timestamp)
+    df['hour'] = df['timestamp_dt'].dt.hour
+    df['day'] = df['timestamp_dt'].dt.day
+    df['dow'] = df['timestamp_dt'].dt.weekday
+    return df
