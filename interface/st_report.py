7b6f717dbe2aee1e3fb035b3244b8f0c6f2808d8
Anton Abilov <anton.abilov@gmail.com>
2021-11-22 12:51:53 -0500

    Added support for saving experiments with '-n' argument
    - Streamlit app loads pickled experiments from /saved_experiments folder

--- interface/st_report.py
+++ interface/st_report.py
@@ -1,12 +1,28 @@
 import streamlit as st
-
+import os
 from experiment_pipeline.data_loader import load_global_feature_set
+import pickle
+import sys
+
+# Hack for pickling evaluation class
+from experiment_pipeline import evaluation
+sys.modules['evaluation'] = evaluation
+
+SAVED_EXPERIMENT_DIR = "saved_experiments/"
 
 def st_report():
     st.header("Report")
+
+    st.header("Explore Experiments")
+    experiments = os.listdir(SAVED_EXPERIMENT_DIR)
+    selected_experiment = st.selectbox("Select experiment", options=experiments)
+    with open(SAVED_EXPERIMENT_DIR + selected_experiment, "rb") as f:
+        loaded_experiment = pickle.load(f)
+
+    st.write(loaded_experiment["eval"].plot_passenger_count_by_time_of_day('test'))
+
     bus_routes = ["B46", "Bx12"]
     selected_bus_route = st.selectbox("Select a bus route", options=bus_routes)
-
     try:
         df_route, stop_dict = load_global_feature_set("./data", selected_bus_route  )
     except:
