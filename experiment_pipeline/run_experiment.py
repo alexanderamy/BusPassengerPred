66f0316e12c6e972e63e538ffc4973b8604adfc9
Anton Abilov <anton.abilov@gmail.com>
2021-11-21 21:44:21 -0500

    Added refit_interval argument
    - I.e. run `python run_experiment.py ../data B46 -t 48H -r 24H`
      to predict 48 hours and refit every 24 hours

--- experiment_pipeline/run_experiment.py
+++ experiment_pipeline/run_experiment.py
@@ -15,28 +15,49 @@ def run_experiment(
     stop_dict,
     dependent_variable="passenger_count",
     split_date=datetime.datetime(year=2021, month=9, day=27),
-    test_period='1D'
-):
-    # Feature selection
-    print("Selecting features...")
-    train, test = feature_extractor_fn(date_train_test_split(global_feature_set, split_date, test_period))
+    test_period='1D',
+    refit_interval=None
+):    
+    if refit_interval is None:
+        train, test = feature_extractor_fn(date_train_test_split(global_feature_set, split_date, test_period))
+        train_x = train.drop(columns=[dependent_variable])
+        train_y = train[dependent_variable]
+        test_x = test.drop(columns=[dependent_variable])
+        test_y = test[dependent_variable]
 
-    train_x = train.drop(columns=[dependent_variable])
-    train_y = train[dependent_variable]
-    test_x = test.drop(columns=[dependent_variable])
-    test_y = test[dependent_variable]
+        print("Fitting model on train set...")
+        model.fit(train_x, train_y)
 
-    # Fit
-    print("Fitting model...")
-    model.fit(train_x, train_y)
+        print("Inference...")
+        train_preds = model.predict(train_x)
+        test_preds = model.predict(test_x)
+        train['passenger_count_pred'] = train_preds
+        test['passenger_count_pred'] = test_preds
+    else:
+        print(f"Refitting every {refit_interval}")
+        initial_split_date = split_date
+        refit_test_sets = []
+        while split_date < initial_split_date + pd.Timedelta(test_period):
+            print("Refitting...")
+            train_refit, test_refit = feature_extractor_fn(date_train_test_split(global_feature_set, split_date, refit_interval))
+            refit_test_sets.append(test_refit)
+            train_x = train_refit.drop(columns=[dependent_variable])
+            train_y = train_refit[dependent_variable]
+            test_x = test_refit.drop(columns=[dependent_variable])
+            test_y = test_refit[dependent_variable]
+            model.fit(train_x, train_y)
 
-    # Inference
-    print("Inference..")
-    train_preds = model.predict(train_x)
-    test_preds = model.predict(test_x)
+            if (split_date == initial_split_date):
+                train = train_refit
+                train_preds = model.predict(train_x)
+                train['passenger_count_pred'] = train_preds
 
-    train['passenger_count_pred'] = train_preds
-    test['passenger_count_pred'] = test_preds
+            test_preds = model.predict(test_x)
+            test_refit['passenger_count_pred'] = test_preds
+
+            split_date += pd.Timedelta(refit_interval)
+        
+        test = pd.concat(refit_test_sets)
 
     # Eval
     return Evaluation(global_feature_set=global_feature_set, train=train, test=test, stop_dict=stop_dict)
@@ -62,6 +83,13 @@ parser.add_argument(
     help='Time period to hold out as test data'
 )
 
+parser.add_argument(
+    '-r',
+    '--refit_interval',
+    default=None,
+    help='Time period for refit'
+)
+
 
 if __name__ == "__main__":
     args = parser.parse_args()
@@ -79,7 +107,8 @@ if __name__ == "__main__":
         feature_set_without_trip_id,
         LassoCV(),
         stop_dict,
-        test_period=args.test_period
+        test_period=args.test_period,
+        refit_interval=args.refit_interval
     )
 
     print("Train evaluation")
