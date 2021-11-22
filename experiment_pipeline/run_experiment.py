d63cea9b328f537259be818f14978d377b030b5b
Anton Abilov <anton.abilov@gmail.com>
2021-11-22 11:32:43 -0500

    Revert "Added test_period argument to specify time period for test data"
    This reverts commit 1b09f47b23085bec41b92b9948cd2bb319e39271.

--- experiment_pipeline/run_experiment.py
+++ experiment_pipeline/run_experiment.py
@@ -13,13 +13,12 @@ def run_experiment(
     feature_extractor_fn,
     model,  
     stop_dict,
-    dependent_variable="passenger_count",
-    split_date=datetime.datetime(year=2021, month=9, day=27),
-    test_period='1D'
+    dependent_variable="passenger_count"
 ):
     # Feature selection
     print("Selecting features...")
-    train, test = feature_extractor_fn(date_train_test_split(global_feature_set, split_date, test_period))
+    split_date = datetime.date(year=2021, month=9, day=27)
+    train, test = feature_extractor_fn(date_train_test_split(global_feature_set, split_date, 1))
 
     train_x = train.drop(columns=[dependent_variable])
     train_y = train[dependent_variable]
@@ -55,35 +54,22 @@ parser.add_argument(
     help='The route number, i.e. "B46"'
 )
 
-parser.add_argument(
-    '-t',
-    '--test_period',
-    default='24H',
-    help='Time period to hold out as test data'
-)
-
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    print("Arguments")
-    print(args)
 
     data_dir = args.data_dir
     route_str = args.route
 
     ## Prepare globlal feature set
-    df_route, stop_dict = load_global_feature_set(data_dir, route_str)
+    df_route, stop_dict = load_global_feature_set()
 
     experiment_eval = run_experiment(
         df_route,
-        feature_set_without_trip_id,
+        feature_set_with_trip_id,
         LassoCV(),
-        stop_dict,
-        test_period=args.test_period
+        stop_dict
     )
 
-    print("Train evaluation")
+    print("Evaluation")
     print(experiment_eval.basic_eval('train'))
-
-    print("Test evaluation")
-    print(experiment_eval.basic_eval('test'))
--- experiment_pipeline/utils.py
+++ experiment_pipeline/utils.py
@@ -1,22 +1,17 @@
-from datetime import datetime, timedelta
-import pandas as pd
+from datetime import timedelta
+from evaluation import Evaluation
 
-def date_train_test_split(segments_data, split_date: datetime, test_period):
+def date_train_test_split(segments_data, split_date, n_test_days):
     """ 
-        Train: day 0 to split_date
-        Test: split_date to split_date + test_period 
+        Train: day 0 to split_date - 1
+        Test: split_date to split_date + n_test_days 
     """
-    # Convert to timezone of dataframe
-    split_date = pd.Timestamp(split_date).tz_localize(segments_data['timestamp'].dt.tz)
-    date_test_end = (split_date + pd.Timedelta(test_period))
-    train = segments_data[segments_data['timestamp'] < split_date].copy()
+    date_test_end = split_date + timedelta(days=n_test_days)
+    train = segments_data[segments_data['timestamp'].dt.date < split_date].copy()
     test = segments_data[
-        (segments_data['timestamp'] >= split_date) &
-        (segments_data['timestamp'] <= date_test_end)
+        (segments_data['timestamp'].dt.date >= split_date) &
+        (segments_data['timestamp'].dt.date <= date_test_end)
     ].copy()
-
-    print(f"fitting on train data until {split_date}: {train.shape[0]:,} rows")
-    print(f"testing from {split_date} to {date_test_end}: {test.shape[0]:,} rows")
 
     return train, test
 
