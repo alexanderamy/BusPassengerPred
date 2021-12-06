63059ff778f9381ca38b433251272edecc2c5959
sanketshahc <sanket.shah.c@gmail.com>
2021-12-06 01:07:01 -0500

    landing text/diagram

--- st_main.py
+++ st_main.py
@@ -5,15 +5,34 @@ from interface. st_eval_harness import st_eval_harness
 
 # Main page
 def render_landing_page():
-    st.subheader("Landing page")
-    st.write("Todo: write a description of the project")
-
-    st.write("We open source a repository for working with the NYC bus data, etc...")
-
-    st.write("Something about the experiments we want to run, i.e. adding weather data.")
-
+    st.header("BusWatcher | Insights")
+    st.subheader("Data Science Toolset")
+    st.write("""
+             BusWatcher Insights is an open source library that aims to provide researchers a 
+             scaffold for data engineering and analysis. Currently it's main focus is on passenger 
+             count data from the NYC bys system. The public repository is a set up to showcase 
+             the best performing models and give insight into experiments. It provides utility methods for fetching data, running experiments for building prediction models and a
+             standardized evaluation suite.
+             
+             The work presented here was born of an endeaver to study the impact of weather data 
+             on bus rider volume. Over the course of the project, the team had built up a substantial 
+             data engineering code base. After many challenges with the initial study direction, 
+             the team decided to refactor and rebuild the code base into a user-friendly public 
+             library. 
+             
+             The library is modular and sufficiently flexible to serve a data science workflow, 
+             allowing a researcher to audit data, perform feature selection, compare models, and 
+             present findings, in a manageable, piecemeal fashion without much overhead.
+             BusWatcher Insights is powered by NYC BusWatcher and other publicly available Urban 
+             data sources. It is a collaboration between students at Cornell Tech and the Urban 
+             Tech Hub.
+            
+             Use the navigation on the left to learn more or explore experiment demos.
+             """
+             )
 
     st.write("**[Github repository](https://github.com/Cornell-Tech-Urban-Tech-Hub/buswatcher-insights/)**")
+    st.image("./interface/images/workflow.png")
 
 PAGES = {
     "Landing page": render_landing_page,
